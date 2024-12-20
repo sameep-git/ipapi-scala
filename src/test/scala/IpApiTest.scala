5c4c630981e1be3843a0bfc4897b1bc627e2cd81
sameep-git <sameep.shah@tcu.edu>
2024-12-20 17:09:11 -0600

    test: added more unit tests for queryOwnIP() and queryIP()
    all tests are passing!

--- src/test/scala/IpApiTest.scala
+++ src/test/scala/IpApiTest.scala
@@ -1,3 +1,20 @@
 package com.ipquery.ipapi
 
 class IpApiTest
+
+class IpApiTest extends AnyFunSuite:
+  test("IpApi.QueryOwnIP") {
+    val result = IpApi.queryOwnIP()
+    assert(result.isRight)
+    assert(result.exists(_.trim.nonEmpty))
+  }
+  test("IpApi.QueryIP") {
+    val result = IpApi.queryIP("8.8.8.8")
+    assert(result.isRight)
+    result match
+      case Right(a) =>
+        assert(a.isp.isDefined)
+        assert(a.location.isDefined)
+        assert(a.risk.isDefined)
+      case Left(_) => assert(0 === 1)
+  }
